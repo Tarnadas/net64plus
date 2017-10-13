69974a5358bb1df63ce2fb94f22aabfafcfdbc3a
Mario <m_reder10@cs.uni-kl.de>
2017-10-13 11:50:39 +0200

    truncate .0 patch version

--- webpack.prod.js
+++ webpack.prod.js
@@ -93,7 +93,7 @@ module.exports = [
     plugins: [
       new webpack.EnvironmentPlugin({
         NODE_ENV: 'production',
-        VERSION: process.env.npm_package_version
+        VERSION: process.env.npm_package_version.slice(-2) === '.0' ? process.env.npm_package_version.slice(0, process.env.npm_package_version.length - 2) : process.env.npm_package_version
       }),
       new webpack.optimize.ModuleConcatenationPlugin(),
       new BabiliPlugin({
