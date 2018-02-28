a6b69f7aaa253981a5d7da40bf2d41bc2c2130c1
Mario Reder <m_reder10@cs.uni-kl.de>
2018-02-28 17:47:27 +0100

    feature(monetization): Get rid of this useless crypto miner

--- src/components/views/FaqView.tsx
+++ src/components/views/FaqView.tsx
@@ -53,22 +53,6 @@ class View extends React.PureComponent<FaqViewProps> {
           text='Got it!'
           onClick={this.onDone}
         />
-        <h2>Monetization</h2>
-        <div style={styles.text}>
-          Net64+ uses JSECoin for monetization. It is a cryptocurrency which is mined while the program is running.
-          At first this might sound like a bad deal for the users, but the idea of JSECoin is to only use resources of the CPU (~1-3%), that would otherwise be wasted.
-          It will also automatically scale with your hardware strength, so weak computers will have the same CPU usage as strong hardware.
-          Please open task manager and check for yourself.<br/><br/>
-          Please also compare it to how you feel about ads as monetization.<br/><br/>
-          This monetization helps us to pay for servers and domains and keeps the project alive.
-          It is the least annoying method for users and please don&#39;t freak up when you read cryptocurrency.
-          Mining cryptopcurrencies in browsers has bad reputation, because of a mining service that was used by companies like TBP.
-          The mining service they were using was mining another cryptocurrency called Monero. To effectively earn money with Monero it must use a lot of computation power.
-          JSECoin is different, because it gets its full potential with only very few resources and the rewards you get is basically limited by their servers, since there is only one mining pool.<br/><br/>
-          We will never abuse your hardware!<br/><br/>
-          If you still want to opt-out of mining, you can do so by clicking <a href='https://server.jsecoin.com/optout/' target='_blank'>here</a> and restarting Net64+.
-          If you want to opt-in please click <a href='https://server.jsecoin.com/optin/' target='_blank'>here</a>.
-        </div>
         <h2 style={{marginTop: '40px'}}>Emulator settings</h2>
         <div style={styles.text}>
           Make sure your emulator settings match the green circles
