f847e3f95fe52112b4966a1e60d11c4327926d59
Mario Reder <m_reder10@cs.uni-kl.de>
2018-05-11 08:11:34 +0200

    fix: Loading indicator for server list

--- src/renderer/components/helpers/ProgressSpinner.tsx
+++ src/renderer/components/helpers/ProgressSpinner.tsx
@@ -2,10 +2,15 @@ import './ProgressSpinner.scss'
 
 import * as React from 'react'
 
-export class ProgressSpinner extends React.PureComponent {
+interface ProgressSpinnerProps {
+  inline?: boolean
+}
+
+export class ProgressSpinner extends React.PureComponent<ProgressSpinnerProps> {
   public render (): JSX.Element {
+    const { inline } = this.props
     return (
-      <div className='progress-spinner-wrapper'>
+      <div className={`progress-spinner-wrapper${inline ? ' progress-spinner-wrapper-inline' : ''}`}>
         <div className='progress-spinner' />
       </div>
     )
