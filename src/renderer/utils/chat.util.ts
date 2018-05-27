bc7f15f3a4c7d3284b1183fb8c0eb2480288ac18
Mario Reder <m_reder10@cs.uni-kl.de>
2018-05-27 16:10:18 +0200

    hotfix: XSS prevention

--- src/renderer/utils/chat.util.ts
+++ src/renderer/utils/chat.util.ts
@@ -1,13 +1,19 @@
+import * as marked from 'marked'
+import { emojify } from 'node-emoji'
+
 import { store } from '..'
 import { addGlobalChatMessage, clearGlobalChatMessages } from '../actions/chat'
 import { ChatMessage } from '../../models/State.model'
 
+const { sanitize } = require('dompurify').default
+
 export function addGlobalMessage (message: string, username: string) {
   const date = new Date()
+  const sanitizedMessage = sanitize(emojify(marked(message.replace(/<.*>/g, ''))))
   const chatMessage: ChatMessage = {
     key: date.getUTCMilliseconds(),
     time: `${String(date.getHours()).padStart(2, '00')}:${String(date.getMinutes()).padStart(2, '00')}:${String(date.getSeconds()).padStart(2, '00')}`,
-    message,
+    message: sanitizedMessage,
     username
   }
   store.dispatch(addGlobalChatMessage(chatMessage))
