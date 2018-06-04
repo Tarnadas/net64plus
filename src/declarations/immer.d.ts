b30dc771406b8917f026dcaac2fd7fdaf4b31114
Mario Reder <m_reder10@cs.uni-kl.de>
2018-06-04 17:13:14 +0200

    hotfix: Check username loaded from save.json

--- src/declarations/immer.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'immer' {
-  import produce from 'immer'
-
-  const immer: <S>(baseState: Readonly<S>, draftFunction: (draftState: S) => void) => Readonly<S>
-  export default immer
-}
