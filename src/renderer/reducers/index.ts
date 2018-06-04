6ee33f8ddbb45dfa4b706c95e7700bfd6cf0ae08
Mario Reder <m_reder10@cs.uni-kl.de>
2018-06-04 17:13:14 +0200

    hotfix: Check username loaded from save.json

--- src/renderer/reducers/index.ts
+++ src/renderer/reducers/index.ts
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware, combineReducers, Store, ReducersMapObject } from 'redux'
 import { routerMiddleware, RouterState } from 'react-router-redux'
 import { History } from 'history'
+import produce from 'immer'
 
 import { save } from './save'
 import { router } from './router'
 import { connection } from './connection'
 import { chat } from './chat'
-import { State, SaveState, ElectronSaveData } from '../../models/State.model'
+import { MIN_LENGTH_USERNAME, MAX_LENGTH_USERNAME } from '../components/views/SettingsView'
+import { State, SaveState, ElectronSaveData, StateDraft } from '../../models/State.model'
 
 export let initialState: State
 
@@ -21,7 +23,15 @@ const APP_SAVE_DATA: ElectronSaveData = {
 }
 
 export function initReducer (history: History, electronSave: SaveState): Store<State> {
-  const appSaveData: ElectronSaveData = Object.assign({}, APP_SAVE_DATA, electronSave.appSaveData)
+  let appSaveData: ElectronSaveData = Object.assign({}, APP_SAVE_DATA, electronSave.appSaveData)
+  const username = appSaveData.username.replace(/\W/g, '')
+  if (
+    username !== appSaveData.username ||
+    username.length < MIN_LENGTH_USERNAME ||
+    username.length > MAX_LENGTH_USERNAME
+  ) {
+    appSaveData = Object.assign({}, APP_SAVE_DATA)
+  }
   const appSavePath: string = electronSave.appSavePath || ''
   initialState = {
     save: {
--- src/renderer/utils/chat.util.ts
+++ src/renderer/utils/chat.util.ts
@@ -2,6 +2,7 @@ import * as marked from 'marked'
 import { emojify } from 'node-emoji'
 
 import { store } from '..'
+import { MAX_LENGTH_USERNAME } from '../components/views/SettingsView'
 import { addGlobalChatMessage, clearGlobalChatMessages } from '../actions/chat'
 import { ChatMessage } from '../../models/State.model'
 
@@ -14,7 +15,7 @@ export function addGlobalMessage (message: string, username: string) {
     key: date.getUTCMilliseconds(),
     time: `${String(date.getHours()).padStart(2, '00')}:${String(date.getMinutes()).padStart(2, '00')}:${String(date.getSeconds()).padStart(2, '00')}`,
     message: sanitizedMessage,
-    username
+    username: sanitize(username).substr(0, MAX_LENGTH_USERNAME)
   }
   store.dispatch(addGlobalChatMessage(chatMessage))
 }
