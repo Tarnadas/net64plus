45d43a1b28d6766fd52f02fba4ddf398aaa74201
Mario Reder <m_reder10@cs.uni-kl.de>
2018-06-08 22:29:02 +0200

    fix: Disables Markdown for user messages and adds sanitization for server description

--- src/models/State.model.ts
+++ src/models/State.model.ts
@@ -36,6 +36,7 @@ export interface ChatMessage {
   time: string
   message: string
   username: string
+  isTrusted: boolean
 }
 
 export interface ChatStateDraft {
--- src/renderer/utils/chat.util.ts
+++ src/renderer/utils/chat.util.ts
@@ -8,14 +8,18 @@ import { ChatMessage } from '../../models/State.model'
 
 const { sanitize } = require('dompurify').default
 
-export function addGlobalMessage (message: string, username: string) {
+export function addGlobalMessage (message: string, username: string, isTrusted = false) {
   const date = new Date()
-  const sanitizedMessage = sanitize(emojify(marked(message.replace(/<.*>/g, ''))))
+  const sanitizedMessage = isTrusted
+    ? sanitize(emojify(marked(message)))
+    : emojify(message)
+  console.log('MSG', message, sanitizedMessage)
   const chatMessage: ChatMessage = {
     key: date.getUTCMilliseconds(),
     time: `${String(date.getHours()).padStart(2, '00')}:${String(date.getMinutes()).padStart(2, '00')}:${String(date.getSeconds()).padStart(2, '00')}`,
     message: sanitizedMessage,
-    username: sanitize(username).substr(0, MAX_LENGTH_USERNAME)
+    username: sanitize(username).substr(0, MAX_LENGTH_USERNAME),
+    isTrusted
   }
   store.dispatch(addGlobalChatMessage(chatMessage))
 }
